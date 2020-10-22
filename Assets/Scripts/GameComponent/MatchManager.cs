c2fa873701d3c7f7dfa98f88b8cc637961a10689
JaussGameDev <jaussgamedev@gmail.com>
2020-10-22 09:16:36 +0200

    MatchManager added

---
+++ Assets/Scripts/GameComponent/MatchManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    [SerializeField] List<Team> teams = new List<Team>();
+    [SerializeField] bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var locTeams = FindObjectsOfType<Team>();
+        foreach(Team tms in locTeams)
+        {
+            if (tms.GetType() == Team.TeamType.Player) teams.Add(tms);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var locTeam = new List<Team>();
+        locTeam.AddRange(teams);
+        foreach(Team tms in locTeam)
+        {
+            if (tms.GetHasLost())
+            {
+                teams.Remove(tms);
+                if (teams.Count <= 1) gameOver = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameComponent/Team.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+    public enum TeamType
+    {
+        Player,
+        IA
+    };
+
+    [SerializeField] private TeamType type = TeamType.Player; 
+    [SerializeField] private string teamName;
+    [SerializeField] private int nbArmyKilled = 0;
+    [SerializeField] private Army army = null;
+    [SerializeField] private bool hasLost = false;
+    [SerializeField] private Home home = null;
+
+    private void Awake()
+    {
+        if (teamName == "Ia") type = TeamType.IA;
+        else home = GetComponentInChildren<Home>();
+        army = GetComponentInChildren<Army>();
+    }
+    public string GetTeamName()
+    {
+        return teamName;
+    }
+    public void LooseGame() 
+    {
+        hasLost = true;
+        army.KillAll();
+    }
+    public void AddKill()
+    {
+        nbArmyKilled ++;
+    }
+    public void SetArmy(Army value)
+    {
+        army = value;
+    }
+    public TeamType GetType()
+    {
+        return type;
+    }
+    public Home GetHome()
+    {
+        return home;
+    }
+    public bool GetHasLost()
+    {
+        return hasLost;
+    }
+}
